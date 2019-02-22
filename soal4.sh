c3572d3a047060d0660f31f744cbfcee35ba03ba
Donny Fitrado <36808083+mbohlahdon@users.noreply.github.com>
2019-02-22 16:49:16 +0700

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -4,4 +4,4 @@ upper=ABCDEFGHIJKLMNOPQRSTUVWXYZ
 lower=abcdefghijklmnopqrstuvwxyz
 DATE=`date '+%H:%M %d-%m-%Y'`
 n=`date '+%H'`
-tr "$upper$lower" "${upper:n}${upper:0:n}${lower:n}${lower:0:n}" <<< `cat /var/log/syslog` > ~/modul1/"$DATE"
+tr "$upper$lower" "${upper:n}${upper:0:n}${lower:n}${lower:0:n}" <<< `cat /var/log/syslog` > ~/sisop1/"$DATE"
