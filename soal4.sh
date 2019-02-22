ef7ad672751efea147d4a6891e96ba15ea31705f
Donny Fitrado <36808083+mbohlahdon@users.noreply.github.com>
2019-02-22 08:13:50 +0700

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -1,5 +1,18 @@
-upper=ABCDEFGHIJKLMNOPQRSTUVWXYZ
-lower=abcdefghijklmnopqrstuvwxyz
-DATE=`date '+%H:%M %d-%m-%Y'`
-n=`date '+%H'`
-tr "$upper$lower" "${upper:n}${upper:0:n}${lower:n}${lower:0:n}" <<< `cat /var/log/syslog` > ~/modul1/"$DATE"
+#!/bin/bash
+
+lowerCase=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+upperCase=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+
+tgl=`date +"%d"`
+bln=`date +"%m"`
+thn=`date +"%Y"`
+jam=`date +"%H"`
+menit=`date +"%M"`
+
+home="/home/hp/ss1/modul1"
+namafile="$home/$jam:$menit $tgl-$bln-$thn.txt"
+
+bawah=${lowerCase[$jam]}
+atas=${upperCase[$jam]}
+
+cat "/var/log/syslog" | tr '[a-z]' "[$bawah-za-$bawah]" | tr '[A-Z]' "[$atas-ZA-$atas]" > "$namafile"
