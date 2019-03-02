b8573d6889b1e4002a5289f4e96f4cce78ffea22
wildangbudhi <37364667+wildangbudhi@users.noreply.github.com>
2019-03-02 23:12:29 +0700

    Bismillah

--- soal4_encrypt.sh
+++ soal4_encrypt.sh
@@ -1,7 +1,13 @@
-hour=`date +"%H"`
-filename=`date +"%H:%M %d-%m-%Y"`
+#!/bin/bash
 
-lowercase="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowerCase=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+upperCase=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 
-cat /var/log/syslog | tr "${lowercase:0:26}${uppercase:0:26}" "${lowercase:$hour:26}${uppercase:$hour:26}" > "/home/hp/sisop1/soal4_encrypt/$filename"
+hour=`date "+%H"`
+
+filename=`date "+%H:%M %d-%m-%Y"`
+
+lower=${lowerCase[${hour:1:2}]}
+upper=${upperCase[${hour:1:2}]}
+
+cat /var/log/syslog | tr '[a-z]' "[$lower-za-$lower]" | tr '[A-Z]' "[$upper-ZA-$upper]" >> encrypted/"$filename".txt
